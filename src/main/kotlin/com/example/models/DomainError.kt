d7b57c7028b0f53b87524dc59515b7af2b9a8d59
masato-bkn <37011138+masato-bkn@users.noreply.github.com>
2026-05-05 14:01:29 +0900

    ValidationFailedErrorを実装

--- src/main/kotlin/com/example/models/DomainError.kt
+++ src/main/kotlin/com/example/models/DomainError.kt
@@ -1,7 +1,9 @@
 package com.example.models
 
+data class FieldError(val field: String, val message: String)
+
 sealed class DomainError(val message: String) {
     data class NotFound(val detail: String) : DomainError(detail)
-
     data class BadRequest(val detail: String) : DomainError(detail)
+    data class ValidationFailedError(val errors: List<FieldError>) : DomainError("FieldError")
 }
