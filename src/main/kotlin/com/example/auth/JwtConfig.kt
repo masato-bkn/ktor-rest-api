e05cdc3db2654f6658871a0f9ae63f404b5d640a
masato-bkn <37011138+masato-bkn@users.noreply.github.com>
2026-05-23 21:35:51 +0900

    JWTトークン生成のJwtConfigを追加

---
+++ src/main/kotlin/com/example/auth/JwtConfig.kt
@@ -0,0 +1,30 @@
+package com.example.auth
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.server.config.ApplicationConfig
+import java.util.Date
+
+const val ONE_HOUR_MILLIS = 3_600_000
+
+class JwtConfig(private val secret: String, val issuer: String, val audience: String, val realm: String) {
+    fun generateToken(userId: Int): String {
+        return JWT.create()
+            .withIssuer(issuer)
+            .withAudience(audience)
+            .withClaim("id", userId)
+            .withExpiresAt(Date(System.currentTimeMillis() + ONE_HOUR_MILLIS))
+            .sign(Algorithm.HMAC256(secret))
+    }
+
+    companion object {
+        fun from(config: ApplicationConfig): JwtConfig {
+            return JwtConfig(
+                secret = config.property("jwt.secret").getString(),
+                issuer = config.property("jwt.issuer").getString(),
+                audience = config.property("jwt.audience").getString(),
+                realm = config.property("jwt.realm").getString(),
+            )
+        }
+    }
+}
