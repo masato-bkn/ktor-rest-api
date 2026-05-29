3d96d930d50a9231b2ac4b415c2b294218334708
masato-bkn <37011138+masato-bkn@users.noreply.github.com>
2026-05-29 23:47:17 +0900

    返却されるトークンがIDを含んでいることをテスト

--- src/test/kotlin/com/example/auth/JwtConfigTest.kt
+++ src/test/kotlin/com/example/auth/JwtConfigTest.kt
@@ -1,8 +1,15 @@
 package com.example.auth
 
+import com.auth0.jwt.JWT
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class JwtConfigTest {
     @Test
-    fun `generateToken - returns new token`() {}
+    fun `generateToken - returns new token including id`() {
+        val token = JwtConfig("secret", "issuer", "audience", "realm").generateToken(1)
+        val decodedToken = JWT.decode(token)
+        val id = decodedToken.getClaim("id").asInt()
+        assertEquals(1, id)
+    }
 }
