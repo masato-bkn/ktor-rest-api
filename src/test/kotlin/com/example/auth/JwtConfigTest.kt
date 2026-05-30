2b2b055bed95a822b8637e656895a5e9ad3c7954
masato-bkn <37011138+masato-bkn@users.noreply.github.com>
2026-05-30 22:41:03 +0900

    JwtConfigのテストを追加

--- src/test/kotlin/com/example/auth/JwtConfigTest.kt
+++ src/test/kotlin/com/example/auth/JwtConfigTest.kt
@@ -1,15 +1,34 @@
 package com.example.auth
 
 import com.auth0.jwt.JWT
+import com.auth0.jwt.interfaces.DecodedJWT
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class JwtConfigTest {
+    private fun decodedToken(userId: Int = 1, issuer: String = "issuer", audience: String = "audience"): DecodedJWT {
+        val token = JwtConfig("secret", issuer, audience, "realm").generateToken(userId)
+        return JWT.decode(token)
+    }
+
     @Test
     fun `generateToken - returns new token including id`() {
-        val token = JwtConfig("secret", "issuer", "audience", "realm").generateToken(1)
-        val decodedToken = JWT.decode(token)
-        val id = decodedToken.getClaim("id").asInt()
-        assertEquals(1, id)
+        val expectedUserId = 100
+        val id = decodedToken(userId = expectedUserId).getClaim("id").asInt()
+        assertEquals(expectedUserId, id)
+    }
+
+    @Test
+    fun `generateToken - returns new token including issuer`() {
+        val expectedIssuer = "expectedIssuer"
+        val issuer = decodedToken(issuer = expectedIssuer).issuer
+        assertEquals(expectedIssuer, issuer)
+    }
+
+    @Test
+    fun `generateToken - returns new token including audience`() {
+        val expectedAudience = "expectedAudience"
+        val audience = decodedToken(audience = expectedAudience).audience
+        assertEquals(listOf(expectedAudience), audience)
     }
 }
